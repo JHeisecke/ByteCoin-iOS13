59b9baf8589b3ce6101fc59035be34086d416c0f
Javier Heisecke <javier.heisecke@bbva.com>
2020-12-30 16:24:41 -0300

    Creates IBOutlets for labels and pickerview

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    
+    @IBOutlet weak var amount: UILabel!
+    @IBOutlet weak var currency: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
